6f83757c828e9a9e2fe0ea348653bd39226bdc44
Rakshitha-Pai <121934659+Rakshitha-Pai@users.noreply.github.com>
2024-05-12 23:20:18 +0530

    A_star.py

--- A_star.py
+++ A_star.py
@@ -1,66 +1,86 @@
-def aStarAlgo(start_node, stop_node, Graph_nodes):
-    open_set = {start_node}
-    closed_set = set()
-    g = {start_node: 0}
-    parents = {}
+def astarAlgo(start_node, stop_node):
+    open_set=set(start_node)
+    closed_set=set()
+    g={}
+    parents={}
     
-    while open_set:
-        n = None
+    g[start_node]=0
+    parents[start_node]=start_node
+    
+    while len(open_set)>0:
+        n=None
+        
         for v in open_set:
-            if n is None or g[v] + heuristic(v) < g[n] + heuristic(n):
-                n = v
-        if n == stop_node or Graph_nodes[n] is None:
-            break
+            if n==None or g[v] + heuristic(v) + g[n] + heuristic(n):
+                n=v
+                
+        if n==stop_node or graph_nodes[n]==None:
+            pass
+        else:
+            for(m,weight) in get_neighbours(n):
+                if m not in open_set and m not in closed_set:
+                    open_set.add(n)
+                    parents[m]=n
+                    g[m]=g[n]+weight
+                else:
+                    if g[m]>g[n]+weight:
+                        g[m]=g[n] +weight
+                        parents[m]=n
+                        if m in closed_set:
+                            closed_set.remove(m)
+                            open_set.add(m)
+                        
+        if n==None:
+            print('Path Doesn\'t Exist!' )
+            return None
+        
+        if n==stop_nodes:
+            path=[]
+            while parents[n]!=n:
+                path.append(n)
+                n=parents[n]
+            path.append(start_node)
+            path.reverse()
+            print('Path Found!',format(path))
+            return path
         open_set.remove(n)
         closed_set.add(n)
-        for (m, weight) in get_neighbors(n, Graph_nodes):
-            if m not in closed_set:
-                if m not in open_set or g[m] > g[n] + weight:
-                    parents[m] = n
-                    g[m] = g[n] + weight
-                    open_set.add(m)
+    print('Path Doesn\'t Exist!')
+    return None
 
-    if n is None:
-        print('Path does not exist!')
-        return None
-
-    path = []
-    while n is not None:
-        path.append(n)
-        n = parents.get(n)
-    return path[::-1]
-
-def get_neighbors(v, Graph_nodes):
-    if v in Graph_nodes:
-        return Graph_nodes[v]
+========================
+def get_neighbours(v):
+    if v in graph_nodes:
+        return graph_nodes[v]
     else:
         return None
-
+============
 def heuristic(n):
-    H_dist = {
-        'A': 11,
-        'B': 6,
-        'C': 5,
-        'D': 7,
-        'E': 3,
-        'F': 6,
-        'G': 5,
-        'H': 3,
-        'I': 1,
-        'J': 0
+    H_dist={
+        'A':10,
+        'B':8,
+        'C':5,
+        'D':7,
+        'E':3,
+        'F':6,
+        'G':5,
+        'H':4,
+        'I':1,
+        'J':0
     }
     return H_dist[n]
-
-Graph_nodes = {
-    'A': [('B', 6), ('F', 3)],
-    'B': [('A', 6), ('C', 3), ('D', 2)],
-    'C': [('B', 3), ('D', 1), ('E', 5)],
-    'D': [('B', 2), ('C', 1), ('E', 8)],
-    'E': [('C', 5), ('D', 8), ('I', 5), ('J', 5)],
-    'F': [('A', 3), ('G', 1), ('H', 7)],
-    'G': [('F', 1), ('I', 3)],
-    'H': [('F', 7), ('I', 2)],
-    'I': [('E', 5), ('G', 3), ('H', 2), ('J', 3)],
+==============
+graph_nodes={
+        'A':[('B',6),('F',3)],
+        'B':[('C',3),('D',2)],
+        'C':[('D',6),('E',5)],
+        'D':[('C',6),('E',8)],
+        'E':[('I',6),('J',5)],
+        'F':[('G',6),('H',7)],
+        'G':[('I',3)],
+        'H':[('I',2)],
+        'I':[('E',5),('J',3)]
+    
 }
-
-print(aStarAlgo('A', 'J', Graph_nodes))
+====================
+astarAlgo('A','J')
